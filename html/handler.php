e82dd84c6e3896398da51f03e7569f3f800ee071
tire-regroover <nick.savolainen@gmail.com>
2023-08-30 15:21:05 -0400

    First commit

---
+++ html/handler.php
@@ -0,0 +1,27 @@
+<?php
+
+function checkret($retval) {
+  if ($retval == 0) {
+    $result = "ok";
+  }
+  else {
+    $result = "fail";
+  }
+  return $result;
+}
+
+switch ($_GET["action"]) {
+  case 'motion_start':
+    exec('/var/www/casey_scripts/start_cam', $output, $retval);
+    echo checkret($retval);
+    break;
+  case 'motion_stop':
+    exec('/var/www/casey_scripts/stop_cam', $output, $retval);
+    echo checkret($retval);
+    break;
+  case 'phpinfo':
+    phpinfo();
+    break;
+}
+
+?>
